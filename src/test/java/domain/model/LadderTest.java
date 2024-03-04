d405da6843a4ab848f57b7fdf982d04d9cd82c8c
tackyu <tack970913@gmail.com>
2024-03-04 15:48:26 +0900

    refactor: 의도를 명확히 함

--- src/test/java/domain/model/LadderTest.java
+++ src/test/java/domain/model/LadderTest.java
@@ -14,22 +14,10 @@ class LadderTest {
     @Test
     @DisplayName("출발점으로부터 사다리를 타고 도착점을 반환한다.")
     void findConsequenceTest() {
-        //given
         Ladder ladder = new Ladder(new Height("2"), 3, () -> true);
-        List<Integer> expected = List.of(0, 1, 2);
-        List<Integer> actual = new ArrayList<>();
 
-        //when
-        int consequenceForPosition0 = ladder.goToConsequence(0, 0);
-        actual.add(consequenceForPosition0);
-
-        int consequenceForPosition1 = ladder.goToConsequence(1, 0);
-        actual.add(consequenceForPosition1);
-
-        int consequenceForPosition2 = ladder.goToConsequence(2, 0);
-        actual.add(consequenceForPosition2);
-
-        //then
-        assertThat(expected).isEqualTo(actual);
+        assertThat(ladder.goToConsequence(0, 0)).isEqualTo(0);
+        assertThat(ladder.goToConsequence(1, 0)).isEqualTo(1);
+        assertThat(ladder.goToConsequence(2, 0)).isEqualTo(2);
     }
 }
